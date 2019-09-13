0b898a3a1253d98a9adf3dcbf736d66d02942d31
SleepingFox <seanloganking@gmail.com>
2019-09-13 22:41:43 +0000

    fix sample range

--- prediction2.py
+++ prediction2.py
@@ -1,8 +1,35 @@
+# import csv
+# import numpy as np
+# from sklearn import linear_model #regression line
+# import matplotlib.pyplot as plt
+# from datetime import datetime
+
+# #Modeling Metrics
+# from sklearn import metrics
+
+from datetime import datetime
 import csv
 import numpy as np
-from sklearn import linear_model #regression line
+import pandas as pd
 import matplotlib.pyplot as plt
-from datetime import datetime
+
+
+#Data import
+from pandas_datareader import data as pdr
+# import fix_yahoo_finance as yf
+
+# !pip install yfinance --upgrade --no-cache-dir
+# yf.pdr_override()
+
+#For mounting to drive
+# from google.colab import drive
+
+
+#Modeling
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
 
 #Modeling Metrics
 from sklearn import metrics
@@ -37,29 +64,22 @@ def get_performance (model_pred):
   print('Mean Squared Error:', metrics.mean_squared_error(y_test, model_pred).round(4))  
   print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, model_pred)).round(4))
   
-  
-  
 def get_plot (model_pred):
   plt.scatter(model_pred, y_test, color="gray")
   plt.plot(y_test, y_test, color='red', linewidth=2)
   plt.show()
 
-
 get_data("AAPL.csv")
-
-# plot all our price data
-# plt.plot(dates, prices)
-# plt.show()
 
 # number of samles with at least 32 entried of data before it.
 # we need past 32 days to learn and make predicitons
-window_size=32
-num_samples=len(dates)-window_size
+window_size = 32
+num_samples = len(prices) - window_size
 
 # get X data
 # each x_data item is an array of 32 days of price data, used to predict the 33rd day's price (y_data)
 x_data = []
-for x in range(num_samples - 1):
+for x in range(num_samples):
     past32Prices = []
     for x2 in range(x, x + window_size):
         past32Prices.append(prices[x])
@@ -67,8 +87,9 @@ for x in range(num_samples - 1):
 
 # get y data
 y_data = []
-for x in range(num_samples - 1):
-    y_data.append(prices[x + window_size + 1])
+for x in range(num_samples):
+    print(x)
+    y_data.append(prices[x + window_size])
 
 
 # we want to use 80% of our data for training, and 20% for testing
