f15366d0edd759223c5fe6084d025b3643f8df4e
SleepingFox <seanloganking@gmail.com>
2019-09-13 18:09:46 +0000

    y data and helper functions

--- prediction2.py
+++ prediction2.py
@@ -4,6 +4,9 @@ from sklearn import linear_model #regression line
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+#Modeling Metrics
+from sklearn import metrics
+
 # Loads price CSV data into dates and prices variables
 dates = []
 prices = []
@@ -27,6 +30,20 @@ def get_data(filename):
             prices.append(float(rows[x][headerFields.index("Adj Close")]))
     return
 
+#Help Functions
+def get_performance (model_pred):
+  #Function returns standard performance metrics
+  print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, model_pred).round(4))  
+  print('Mean Squared Error:', metrics.mean_squared_error(y_test, model_pred).round(4))  
+  print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, model_pred)).round(4))
+  
+  
+  
+def get_plot (model_pred):
+  plt.scatter(model_pred, y_test, color="gray")
+  plt.plot(y_test, y_test, color='red', linewidth=2)
+  plt.show()
+
 
 get_data("AAPL.csv")
 
@@ -40,17 +57,18 @@ window_size=32
 num_samples=len(dates)-window_size
 
 # get X data
+# each x_data item is an array of 32 days of price data, used to predict the 33rd day's price (y_data)
 x_data = []
 for x in range(num_samples - 1):
     past32Prices = []
-    for x2 in range(x,x+32):
+    for x2 in range(x, x + window_size):
         past32Prices.append(prices[x])
     x_data.append(past32Prices)
 
 # get y data
 y_data = []
 for x in range(num_samples - 1):
-    y_data.append(prices[x+33])
+    y_data.append(prices[x + window_size + 1])
 
 
 # we want to use 80% of our data for training, and 20% for testing
@@ -63,3 +81,11 @@ x_train = x_data[:ind_split]
 y_train = y_data[:ind_split]
 x_test = x_data[ind_split:]
 y_test = y_data[ind_split:]
+
+
+# base tests
+y_pred_lag=np.roll(y_test,1)
+get_performance(y_pred_lag)
+get_plot(y_pred_lag)
+
+
