755c7808a263cffb274de7fd389d0ca342cba2c9
SleepingFox <seanloganking@gmail.com>
2019-09-13 17:14:47 +0000

    start of data preprocessing

--- prediction2.py
+++ prediction2.py
@@ -4,9 +4,9 @@ from sklearn import linear_model #regression line
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+# Loads price CSV data into dates and prices variables
 dates = []
 prices = []
-
 def get_data(filename):
     with open(filename, 'r') as csvfile:
         csvreader = csv.reader(csvfile)
@@ -27,28 +27,25 @@ def get_data(filename):
             prices.append(float(rows[x][headerFields.index("Adj Close")]))
     return
 
-def to_integer(dt_time):
-    return 10000*dt_time.year + 100*dt_time.month + dt_time.day
-
-# def show_plot(dates, prices):
-#     linear_mod = linear_model.LinearRegression()
-#     dates = np.reshape(dates, (len(dates), 1))
-
-
 
 get_data("AAPL.csv")
 
-print(dates[0])
-print(prices[0])
-
-# plt.plot([1,2,5,4])
+# plot all our price data
+# plt.plot(dates, prices)
 # plt.show()
-print(prices)
-plt.plot(dates, prices)
-plt.show()
 
+# number of samles with at least 32 entried of data before it.
+# we need past 32 days to learn and make predicitons
+window_size=32
+num_samples=len(dates)-window_size
 
-# data = range(100)
-# dataRange = range(len(data))
-# plt.scatter(dataRange, data)
-# plt.show()
+# we want to use 80% of our data for training, and 20% for testing
+# This finds the array index where we should split our data
+split_fraction = 0.8
+ind_split=int(split_fraction * num_samples)
+
+# set trainging and test data
+x_train = dates[:ind_split]
+y_train = prices[:ind_split]
+x_test = dates[ind_split:]
+y_test = prices[ind_split:]
